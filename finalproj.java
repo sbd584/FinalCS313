30e655dc7863c1c147aaee03077a9f13c466eebf
sbd584 <32025147+sbd584@users.noreply.github.com>
2018-05-03 11:28:50 -0500

    Update finalproj.java

--- finalproj.java
+++ finalproj.java
@@ -1,3 +1,154 @@
+class Music_note {
+float x,y,q,yd,xd;
+PShape musicN;
+Music_note(float x, float y,float q, float yd, float xd){
+  // Creating Music Notes
+  this.x = x;
+  this.y = y;
+  this.q = q;
+  this.yd = yd;
+  this.xd = xd;
+
+  musicN = createShape(GROUP);
+
+  noStroke();
+  fill(0,180,0);
+
+  musicN.addChild(createShape(ELLIPSE,25 + q,210,80,80));
+  musicN.addChild(createShape(ELLIPSE,150 + q,210,80,80));
+  musicN.addChild(createShape(RECT,25 + q,50,50,200,7));
+  musicN.addChild(createShape(RECT,150 + q,50,50,200,7));
+  musicN.addChild(createShape(RECT,25 + q,50,175,50,7));
+
+  //Wings
+  fill(255,223,0);
+  musicN.addChild(createShape(RECT,60 + q,80,180,5,50,50,0,0));
+  musicN.addChild(createShape(RECT,80 + q,85,160,5));
+  musicN.addChild(createShape(RECT,100 + q,90,140,5));
+  musicN.addChild(createShape(RECT,120 + q,95,120,5));
+  musicN.addChild(createShape(ARC,70 + q,85,20,15,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,90 + q,90,20,20,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,110 + q,95,20,25,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,130 + q,100,20,30,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,150 + q,100,20,35,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,170 + q,100,20,40,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,190 + q,100,20,45,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,210 + q,100,20,55,0, PI, CHORD));
+  musicN.addChild(createShape(ARC,230 + q,100,20,70,0, PI, CHORD));
+
+  }
+
+  // public Void Music Fill
+
+  public void MusicMove(int music_timer){
+    float mt = music_timer/10;
+    //this.x = 400
+    if(mt < 30){
+      this.y += 2.2;
+      //this.x = 400 + 10*mt;
+      if(mt < 7.5){
+        this.x += 1.8;
+      }
+      else if(mt < 15){
+        this.x -= 1.8;
+      }
+      else if(mt < 22.5){
+        this.x -= 1.8;
+      }
+      else{
+        this.x += 1.8;
+      }
+    }
+    else{
+      this.y -= 2.2;
+      //this.x = 700 - 10*mt;
+      if(mt < (30 + 7.5)){
+        this.x += 1.8;
+      }
+      else if(mt < (30 + 15)){
+        this.x -= 1.8;
+      }
+      else if(mt < (30 + 22.5)){
+        this.x -= 1.8;
+      }
+      else{
+        this.x += 1.8;
+      }
+  }
+
+  }
+
+  public void minis(int music_timer){
+    float mt = music_timer/50;
+
+
+    noStroke();
+
+    if (mt < 3){
+      fill(255,0,255);
+      yd -= .5;
+      xd -= .5;
+      if (mt <= 1.5){
+        fill(255);
+      }
+    }
+    else if (mt < 6){
+      fill(255,255,0);
+      yd += .5;
+      xd -= .5;
+      if (mt <= 4.5){
+        fill(0,255,255);
+      }
+    }
+    else if (mt < 9){
+      fill(255,255,0);
+      yd += .5;
+      xd += .5;
+      if (mt <= 7.5){
+        fill(255,0,255);
+      }
+    }
+
+    else if (mt <= 12){
+      fill(0);
+      yd -= .5;
+      xd += .5;
+      if (mt <= 10.5){
+        fill(0,255,255);
+      }
+    }
+    else{
+      fill(0,255,255);
+      yd = 0;
+      xd = 0;
+
+    }
+
+
+    beginShape(GROUP);
+    // Baby Note
+
+    ellipse(x+175 + q+xd+25,y+175+yd,20.0,20.0);
+    ellipse(x+200 + q+xd+25,y+175+yd,20.0,20.0);
+    rect(x+175 + q+xd+25,y+140+yd,10,35,7);
+    rect(x+200 + q+xd+25,y+140+yd,10,35,7);
+    rect(x+175 + q+xd+25,y+140+yd,35,10,7);
+
+
+
+  }
+
+
+  public void display(int music_timer){
+
+    this.MusicMove(music_timer);
+    shape(musicN,this.x,this.y);
+    this.minis(music_timer);
+  }
+}
+
+
+
 class Middle_key{
 float x,y,c;
 PShape mk;
@@ -165,7 +316,7 @@ float record = 0;
 float record_dot = 0;
 float stop = 0;
 float play = 0;
-float GUI_change = 2;
+float GUI_change = 1;
 PFont font;
 String string1 = " Hello and Welcome ";
 String string2 = " Click To Continue ";
@@ -173,6 +324,9 @@ int gui2Timer;
 int gui2Timer2;
 int gui2TimerCycle;
 int textWidth;
+int music_timer;
+Music_note note1;
+Music_note note2;
 
 void setup() {
   size(1800, 1000);
@@ -247,6 +401,9 @@ void setup() {
   wave31 = new Wave(200, 1600, 325, 100, .1,.2);
   wave32 = new Wave(200, 1600, 325, 100, .1,.1);
 
+  note1 = new Music_note(150,20,50,0,0);
+  note2 = new Music_note(150,20,1200,0,0);
+
 }
 
 void draw() {
@@ -266,6 +423,9 @@ void draw() {
     int height1 = 200;
     int center1 = 575;
     int rate1 = 100;
+
+    note1.display(music_timer);
+    note2.display(music_timer);
 
 // int(gui2Timer/20)
     if (string1.length() > string_char){
@@ -291,6 +451,13 @@ void draw() {
 
       text(string2,500,900);
 
+    }
+
+
+
+    music_timer += 1;
+    if(music_timer%600 == 0){
+      music_timer = 0;
     }
 
   }
